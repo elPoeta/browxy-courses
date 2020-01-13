9a49890f3603fd35d2086f47c68cad8f134aae12
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-01-13 18:30:05 -0300

    task edit create course list courses

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 const root = document.querySelector('#root');
+const courses = [
+  {institution: undefined, name: undefined, year: undefined, division:undefined},
+  {institution:"ITFS 16",name:"ITFS 16 Dialog logica 2020A",year:2020,division:"A"},
+  {institution:"ITFS 16",name:"ITFS 16 Data Base 2020A",year:2020,division:"A"}
+]; 
 const home = () =>{
   root.innerHTML = '<h2>Home</h2>';
 }
-const createCourses = () =>{
+const createEditCourse = (id) =>{
+    const {institution,name,year,division} = courses[id];
   
   const template =
   `<section class="container-form">
   <h2 class="brand"><span>Browxy</span> courses</h2>
   <div class="wrapper-form">
     <div class="side-info">
-      <h3>Browxy Course</h3>
+      <h3>Course</h3>
       <ul>
-      <li>Edit Course</li>
-      <li>Create Tasks</li>
+      <li onclick="teacherListCourses();">My Courses</li>
+      <li onclick="createEditTask();">Create Task</li>
       </ul>
      
     </div>
@@ -21,19 +27,19 @@ const createCourses = () =>{
       <form>
         <p>
           <label>Institution</label>
-          <input type="text" name="institution" >
+          <input type="text" name="institution" value="${institution || ''}">
         </p>
         <p>
           <label>Name</label>
-          <input type="text" name="name">
+          <input type="text" name="name" value="${name || ''}">
         </p>
         <p>
           <label>Year</label>
-          <input type="number" name="year" value=${new Date().getFullYear()}>
+          <input type="number" name="year" value=${year || new Date().getFullYear()}>
         </p>
         <p>
           <label>Division</label>
-          <input type="text" name="division">
+          <input type="text" name="division" value="${division || ''}">
         </p>
         <p class="full">
           <label>Description</label>
@@ -48,4 +54,96 @@ const createCourses = () =>{
   </section>
   `;
   root.innerHTML = template;
+}
+
+const teacherListCourses = () =>{
+  const template =
+  `<h2>Teacher Courses</h2>
+   <section class="teacher-courses">
+    <ul>
+     <li> 
+       <ul class="teacher-course-cell teacher-course-header">
+       <li>Name</li>
+       <li>Year</li>
+       <li>Division</li>
+       <li>Status</li>
+       <li>Students</li>
+       <li>Actions</li>    
+       </ul>
+     </li> 
+     <li> 
+       <ul class="teacher-course-cell teacher-course-row">
+       <li>ITFS 16 Dialog Logica 2020A</li>
+       <li>2020</li>
+       <li>A</li>
+       <li><div class="status status-active"></div> Active</li>
+       <li>15</li>
+       <li><span class="action-course action-course-edit" onclick="createEditCourse(1);">Edit</span> - <span class="action-course action-course-delete">Delete</span></li>      
+       </ul>
+     </li> 
+     <li> 
+       <ul class="teacher-course-cell teacher-course-row">
+       <li>ITFS 16 Data Base 2020A</li>
+       <li>2020</li>
+       <li>A</li>
+       <li><div class="status status-active"></div> Active</li>
+       <li>15</li>
+       <li><span class="action-course action-course-edit" onclick="createEditCourse(2);">Edit</span> - <span class="action-course action-course-delete">Delete</span></li>   
+       </ul>
+     </li> 
+    </ul>
+   </section>
+  `;
+  root.innerHTML = template;
+}
+const createEditTask = () => {
+  const template =
+  `<section class="container-form">
+  <h2 class="brand"><span>Browxy</span> courses</h2>
+  <div class="wrapper-form">
+    <div class="side-info">
+      <h3>Task</h3>
+      <ul>
+      <li onclick="teacherListCourses();">My Courses</li>
+      <li onclick="createEditTask();">New Task</li>
+      </ul>
+     
+    </div>
+    <div class="course-form">
+      <h3>Create Task</h3>
+      <form>
+        <p>
+          <label>Name</label>
+          <input type="text" name="name" >
+        </p>
+        <p>
+        <label>Unit</label>
+        <input type="number" name="unit" >
+      </p>
+        <p>
+          <label>Task#</label>
+          <input type="number" name="task" >
+        </p>
+        <p>
+        <label>ProgramLink id</label>
+        <input type="number" name="programLink" >
+      </p>
+      <p>
+      <input type="text" name="hiddenTestCaseFile" hidden >
+    </p>
+        <p class="full">
+          <button>Add Task</button>
+        </p>
+      </form>
+    </div>
+  </div>
+  </section>
+  `;
+
+  root.innerHTML = template;
+}
+
+const editTask = () => {
+  
+  root.innerHTML = 'Edit Task';
 }
